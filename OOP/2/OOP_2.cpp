21bb9a0684b13deecd9c868cc3aaa765d9f09477
Maria_Grebenyuk <grebenyuk994@gmail.com>
2022-12-01 15:03:52 +0100

    lesson 2

---
+++ OOP/2/OOP_2.cpp
@@ -0,0 +1,82 @@
+﻿#include <iostream>
+//Создать класс Person (человек) с полями: имя, возраст, пол и вес. 
+//Определить методы переназначения имени, изменения возраста и веса. 
+//Создать производный класс Student (студент), имеющий поле года обучения. 
+//Определить методы переназначения и увеличения этого значения. 
+//Создать счетчик количества созданных студентов. 
+//В функции main() создать несколько (не больше трёх) студентов. 
+//Вывести информацию о них.
+
+class Person {
+public:
+	Person(std::string _name, int _age, char _sex, float _weight) {
+		name = _name;
+		age = _age;
+		sex = _sex;
+		weight = _weight;
+	}
+	Person() = default;
+
+	void print() {
+		std::cout << "name: " << name << "\n" << "age: " << age << "\n" << "sex: " << sex << "\n" << "weight: " << weight << std::endl;
+	}
+	void setName(std::string _name) {
+		name = _name;
+	}
+	void setAge(int _age) {
+		age = _age;
+	}
+	void setWeight(float _weight) {
+		weight = _weight;
+	}
+
+private:
+	std::string name = "None";
+	int age = NULL;
+	char sex = ' ';
+	float weight = NULL;
+};
+
+class Student : public Person {
+public:
+	Student(std::string _name, int _age, char _sex, float _weight, int _year) : Person(_name, _age, _sex, _weight) {
+		year = _year;
+		amountOfStudents++;
+	}
+	Student() { amountOfStudents++; };
+	
+	void setYear(int _year) {
+		year = _year;
+	}
+	void increaseYear() {
+		year++;
+	}
+	void print() {
+		Person::print();
+		std::cout << "year: " << year << std::endl << std::endl;
+	}
+	friend void showAmountOfStudents();
+private:
+	int year = 0;
+	static int amountOfStudents;
+};
+
+int Student::amountOfStudents = 0;
+void showAmountOfStudents() {
+	std::cout << "Amount of students created: " << Student::amountOfStudents << std::endl;
+}
+
+int main()
+{
+
+	Student Tommy("Tommy", 33, 'M', 81.3, 1);
+	Student Masha("Masha", 28, 'F', 65, 2);
+
+	Tommy.print();
+	Masha.print();
+
+	showAmountOfStudents();
+
+	return 0;
+}
+
---
+++ OOP/2/OOP_2_2.cpp
@@ -0,0 +1,61 @@
+﻿//2.Создать классы Apple(яблоко) и Banana(банан), которые наследуют класс Fruit(фрукт).
+//У Fruit есть две переменные - члена: name(имя) и color(цвет).
+//Добавить новый класс GrannySmith, который наследует класс Apple.
+
+//3. Должен быть класс, представляющий колоду, а так же базовый класс участника от которого будут производные классы дилер и игрок. Класс суммы денег игрока и производный от него класс ставки. 
+
+#include <iostream>
+
+class Fruit {
+public:
+    std::string getName() {
+        return name;
+    }
+    std::string getColor() {
+        return color;
+    }
+protected:
+    std::string name = "fruit";
+    std::string color = "fruity";
+};
+
+class Apple : public Fruit {
+public:
+    Apple() {
+        name = "apple";
+    }
+    Apple(std::string _color) {
+        name = "apple";
+        color = _color;
+    }
+};
+
+class Banana : public Fruit{
+public:
+    Banana() {
+        name = "banana";
+        color = "yellow";
+    }
+};
+
+class GrannySmith : public Apple {
+public:
+    GrannySmith() { 
+        name = "Granny Smith apple"; 
+        color = "green";
+    }
+};
+
+int main()
+{
+    Apple a("red");
+    Banana b;
+    GrannySmith c;
+
+    std::cout << "My " << a.getName() << " is " << a.getColor() << ".\n";
+    std::cout << "My " << b.getName() << " is " << b.getColor() << ".\n";
+    std::cout << "My " << c.getName() << " is " << c.getColor() << ".\n";
+
+    return 0;
+}
+
