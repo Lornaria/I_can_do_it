9b0f8ebb36b0b65d7d62150b9cc82b0ba57d5459
Maria_Grebenyuk <grebenyuk994@gmail.com>
2023-01-12 20:08:57 +0100

    Moments lesson 5 task 2

--- Moments/5/M_5.cpp
+++ Moments/5/M_5.cpp
@@ -4,6 +4,8 @@
 #include <unordered_set>
 #include <list>
 #include <sstream>
+#include <map>
+#include <algorithm>
 
 using std::cout;
 using std::endl;
@@ -21,6 +23,42 @@ void unicWords(T _begin, T _end) {
 //2. Используя ассоциативный контейнер, напишите программу, которая будет считывать данные введенные пользователем из стандартного потока ввода и разбивать их на предложения.
 //Далее программа должна вывести пользователю все предложения, а также количество слов в них, отсортировав предложения по количеству слов.
  
+void Task2() {
+    std::vector<std::string> vec;
+    std::string text;
+    std::stringstream ss;
+    cout << "Task2. Your text: ";
+    while (std::cin >> text) {
+        if (std::cin.peek() == 10) { 
+            ss << text;
+            break; 
+        }
+        ss << text << " ";
+    }
+    text.clear();
+    std::multimap<std::string, int> myMap;
+    auto countWords = [](std::string& _text) { //лямбда функция для подсчета кол-ва слов в тексте
+        std::stringstream tempSS(_text);
+        int count = 0;
+        std::string oneWord;
+        while (tempSS >> oneWord) {
+            ++count;
+        }
+        return count;
+    };
+    while (std::getline(ss, text, '.'))
+    {
+        myMap.insert({ text + ".", countWords(text)});
+    }
+    std::vector<std::pair<std::string, int>> sentences(myMap.begin(), myMap.end());
+    std::sort(sentences.begin(), sentences.end(), [](const auto& l, const auto& r) {
+        return l.second < r.second;
+        });
+    for (auto [sentence, value] : sentences) {
+        cout << sentence << " (Amount of words: " << value << ")" << endl;
+    }
+}
+
 int main()
 {
     //1.
@@ -31,5 +69,7 @@ int main()
     std::list<std::string> l{ "wo", "wo", "rd", "word", "rd", "word", "hehe" };
     cout << endl;
     unicWords(l.begin(), l.end());
+    cout << endl;
 
+    Task2();
 }
